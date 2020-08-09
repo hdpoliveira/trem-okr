2bb91542fc16ab8b08f66771fc3de3254548af9b
hdpoliveira <hdpoliveira@gmail.com>
2020-08-08 21:26:57 -0300

    Restablish routing

--- src/App.vue
+++ src/App.vue
@@ -3,20 +3,18 @@
         <AppBar />
 
         <v-main>
-            <HelloWorld />
+            <router-view></router-view>
         </v-main>
     </v-app>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "./components/HelloWorld.vue";
 import AppBar from "./components/AppBar.vue";
 
 @Component({
     components: {
         AppBar,
-        HelloWorld,
     },
 })
 export default class App extends Vue {}
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
     <div class="home">
-        <img alt="Vue logo" src="../assets/logo.png" />
+        <img alt="Trem logo" src="../assets/logo.png" />
         <HelloWorld msg="Welcome to Your Vue.js App" />
     </div>
 </template>
 
 <script>
-// @ is an alias to /src
+import { Component, Vue } from "vue-property-decorator";
 import HelloWorld from "@/components/HelloWorld.vue";
 
-export default {
-    name: "Home",
+@Component({
     components: {
         HelloWorld,
     },
-};
+})
+export default class Home extends Vue {}
 </script>
